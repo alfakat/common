6fcfe366ffb4dd134f228330e1f9e1e41254dc14
Kate <44461784+Sukaria@users.noreply.github.com>
2021-07-19 12:56:20 +0300

    Add files via upload
    - replaced queries variables by loop
    - remove prints

--- db_count.py
+++ db_count.py
@@ -11,7 +11,6 @@ try:
 except pymysql.Error as e:
     print("Error reading data ", e)
 
-
 # creating file with results
 def saving_results():
     current_os = platform.system().lower()
@@ -38,23 +37,14 @@ if __name__ == '__main__':
 select_query = "SELECT count(*) FROM videodb.main_view "
 obj_conf = "WHERE ObjectRectConfidence = "
 obj_type = " and ObjectType = "
-hp_yaw = "WHERE HeadPoseYawConfidence = "
-hp_pitch = "WHERE HeadPosePitchConfidence = "
-hp_roll = "WHERE HeadPoseRollConfidence = "
-landm = "WHERE LandmarksConfidence = "
-eyes = "WHERE EyesStateConfidence = "
-eyel_landm = "WHERE EyeLandmarksLConfidence = "
-eyer_landm = "WHERE EyeLandmarksRConfidence = "
-gaze = "WHERE EyeGazeConfidence = "
-eye_wear = "WHERE EyeWearConfidence = "
+pref_str = 'WHERE '
+suf_str = 'Confidence = '
+features = ['HeadPoseYaw', 'HeadPosePitch', 'HeadPoseRoll', 'Landmarks', 'EyesState', 'EyeLandmarksL', 'EyeLandmarksR', 'EyeGaze', 'EyeWear', 'Age', 'Gender',
+             'SeatBeltMarks', 'MouthLandmarks', 'EyeGazeLid', 'IrisMarks']
+features_synth = ['HeadPoseYaw', 'HeadPosePitch', 'HeadPoseRoll', 'Landmarks', 'EyesState', 'EyeLandmarksL', 'EyeLandmarksR', 'EyeGaze', 'EyeWear',
+                  'MouthLandmarks', 'EyeGazeLid', 'IrisMarks']
 mask_yes = "WHERE Mask = 'YES'"
 mask_no = "WHERE Mask = 'NO'"
-age = "WHERE AgeConfidence = "
-gender = "WHERE GenderConfidence = "
-seat_belt = "WHERE SeatBeltMarksConfidence = "
-mouth_land = "WHERE MouthLandmarksConfidence = "
-gaze_lid = "WHERE EyeGazeLidConfidence = "
-iris = "WHERE IrisMarksConfidence = "
 
 # rows
 confidence = ["'HIGH'", "'MEDIUM'", "'INVALID'", "'NONE'"]
@@ -70,62 +60,23 @@ for j in confidence:
         cur.execute(select_query + obj_conf + j + obj_type + i)
         print(cur._last_executed)
         print(cur.fetchone())
+        cur.execute(select_query + mask_yes)
+        print(cur._last_executed)
+        print(cur.fetchone())
+        cur.execute(select_query + mask_no)
+        print(cur._last_executed)
+        print(cur.fetchone())
 
 for j in confidence:
-    cur.execute(select_query + hp_yaw + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + hp_pitch + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + hp_roll + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + landm + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eyes + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eyel_landm + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eyer_landm + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + gaze + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eye_wear + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + mask_yes)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + mask_no)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + age + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + gender + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + seat_belt + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + mouth_land + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + gaze_lid + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + iris + j)
-    print(cur._last_executed)
-    print(cur.fetchone())
+    for f in features:
+        cur.execute(select_query + pref_str + f + suf_str + j + synth_query)
+        print(pref_str + sub + suf_str) #debug
+        print(cur._last_executed)
+        print(cur.fetchone())
 
 print('\n===== Synthetic data =====\n')
 
+# Synthetic objects counting
 
 def synthetic():
     cur = conn.cursor()
@@ -137,11 +88,9 @@ def synthetic():
     synth_coll = str(synth).replace('(', '').replace(',)', '').replace(')', '')
     synth_query = ' and CollectionID in (' + synth_coll + ')'
 
-
 if __name__ == '__main__':
     synthetic()
 
-# Synthetic objects counting
 for j in confidence:
     obj_type_face = " and ObjectType = 'face'"
     cur.execute(select_query + obj_conf + j + obj_type_face + synth_query)
@@ -149,73 +98,11 @@ for j in confidence:
     print(cur.fetchone())
 
 for j in confidence:
-    cur.execute(select_query + hp_yaw + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + hp_pitch + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + hp_roll + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + landm + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eyes + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eyel_landm + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eyer_landm + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + gaze + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + eye_wear + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + mask_yes + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + mask_no + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + age + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + gender + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + seat_belt + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + mouth_land + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + gaze_lid + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
-    cur.execute(select_query + iris + j + synth_query)
-    print(cur._last_executed)
-    print(cur.fetchone())
+    for s in features_synth:
+        cur.execute(select_query + pref_str + s + suf_str + j + synth_query)
+        print(cur._last_executed)
+        print(cur.fetchone())
 
 sys.stdout.close()
 cur.close()
 conn.close()
-
-'''
-# to do
-# initializing list
-test_list = ['Landmarks', 'EyeLandmarksL']
-
-# initializing append_str
-pref_str = '"WHERE '
-suf_str = 'Confidence = "'
-
-# Append suffix / prefix to strings in list
-for sub in test_list:
-    result = pref_str + sub + suf_str
-    print(result)
-'''
